bcde203c7fe5cbc3acc0ce6e2bf56f67d0adc957
Sema-qq <PO-2-06_024@mail.ru>
2017-10-06 11:19:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //вернуться на текущую страницу
 window.location.reload();
+
+//если в битриксе на одном шаблоне несколько сайтов, и нужен разный вывод виджетов лидбека
+
+var code;
+if(window.location.hostname == "urlsite"){
+  code = 'code1';
+}
+else if(window.location.hostname == "urlsite2"){
+  code = 'code2';
+}
+var _emv = _emv || [];
+  _emv['campaign'] = code;
+  (function() {
+    var em = document.createElement('script'); em.type = 'text/javascript'; em.async = true;
+    em.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'leadback.ru/js/leadback.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(em, s);
+  })();
+
